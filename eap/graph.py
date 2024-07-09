3e2155053efe55c5051c35672dc14b5b0e739989
Michael Hanna <mh2parker@gmail.com>
2024-07-09 15:28:12 +0000

    finalizing loading from / saving to pt

--- eap/graph.py
+++ eap/graph.py
@@ -410,6 +410,13 @@ class Graph:
 
     @classmethod
     def from_json(cls, filename):
+        """
+        Load a Graph object from a JSON file.
+        The JSON should have the following keys:
+        1. 'cfg': Configuration dictionary, containing similar values to a TLens configuration object.
+        2. 'nodes': Dict[str, bool] which maps a node name (i.e. 'm11' or 'a0.h11') to a boolean value, indicating if the node is part of the circuit.
+        3. 'edges': Dict[str, Dict] which maps an edge name ('node->node') to a dictionary contains values 
+        """
         with open(filename, 'r') as f:
             d = json.load(f)
         g = Graph.from_model(d['cfg'])
@@ -419,6 +426,35 @@ class Graph:
         for name, info in d['edges'].items():
             g.edges[name].score = info['score']
             g.edges[name].in_graph = info['in_graph']
+
+        return g
+    
+    @classmethod
+    def from_pt(cls, filename):
+        """Load a graph object from a pytorch-serialized file.
+        The file should contain a dict with the following items -
+        1. 'cfg': Configuration dictionary, containing similar values to a TLens configuration object.
+        2. 'src_nodes': Dict[str, bool] which maps a node name (i.e. 'm11' or 'a0.h11') to a boolean value, indicating if the node is part of the circuit.
+        3. 'dst_nodes': List[str] containing the names of the possible destination nodes, in the same order as the edges tensor.
+        4. 'edges': torch.tensor[n_src_nodes, n_dst_nodes], where each value in (src, dst) represents the edge score between the src node and dst node.
+        5. 'edges_in_graph': torch.tensor[n_src_nodes, n_dst_nodes], where each value in (src, dst) represents if the edge is in the graph or not.
+        """
+        d = torch.load(filename)
+        assert all([k in d.keys() for k in ['cfg', 'src_nodes', 'dst_nodes', 'edges', 'edges_in_graph']]), f"Bad torch circuit file format. Found keys - {d.keys()}, missing keys - {set(['cfg', 'src_nodes', 'dst_nodes', 'edges', 'edges_in_graph']) - set(d.keys())}"
+        assert d['edges'].shape == d['edges_in_graph'].shape == (len(d['src_nodes']), len(d['dst_nodes'])), "Bad edges array shape"
+
+        g = Graph.from_model(d['cfg'])
+
+        for name, in_graph in d['src_nodes'].items():
+            g.nodes[name].in_graph = in_graph
+
+        # Enumerate over the tensor and fill the edge values in the graph
+        for src_idx, src_name in enumerate(d['src_nodes']):
+            for dst_idx, dst_name in enumerate(d['dst_nodes']):
+                edge_name = f'{src_name}->{dst_name}'
+                if edge_name in g.edges.keys():
+                    g.edges[edge_name].score = d['edges'][src_idx, dst_idx]
+                    g.edges[edge_name].in_graph = d['edges_in_graph'][src_idx, dst_idx]
 
         return g
     
